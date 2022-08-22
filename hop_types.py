6081af882590cf98a8f29454dac64dde6c80495d
meetowl <mishuk2000@gmail.com>
2022-08-22 02:27:03 +0100

    Tuples in type system

--- hop_types.py
+++ hop_types.py
@@ -1,4 +1,7 @@
 from enum import Enum
+import hop_types
+import functools
+
 class Base:
     class Attributes(Enum):
         EQ =     0b01
@@ -23,7 +26,6 @@ class Base:
             return out
 
 
-
     def __init__(self, width=32, name='base', attributes=[]):
         self.width = width
         self.name = name
@@ -38,3 +40,25 @@ class Base:
 
     def __str__(self):
         return f'{self.name}-{self.width}'
+
+class Tuple:
+    def __init__(self, *elements):
+        # Make sure everything in arguments is an instance of Base
+        self.initialised = False
+
+    def from_list(self, l):
+        assert not self.initialised
+
+        all_base = functools.reduce(lambda a,b: a & b,
+                                    map(lambda a: isinstance(a, hop_types.Base), l))
+        if not all_base:
+            raise TypeError("Must be HoP type") from Exception
+
+        self.elements = tuple(l)
+        self.initialised = True
+
+    def __str__(self):
+        out = '('
+        for e in self.elements:
+            out += f'{str(e)},'
+        return out[:-1] + ')'
