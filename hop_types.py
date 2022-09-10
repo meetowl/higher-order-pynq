0a389ee72b64f91dead4e259400024c77c2876f7
meetowl <mishuk2000@gmail.com>
2022-09-10 12:21:37 +0100

    Simplification of hop_types to V3 spec
    Re-focus to V3 specification, don't implement things we don't consider yet.

--- hop_types.py
+++ hop_types.py
@@ -3,62 +3,32 @@ import hop_types
 import functools
 
 class Base:
-    class Attributes(Enum):
-        EQ =     0b01
-        ORD =    0b10
-
-        def to_num(attr):
-            out = 0;
-            if type(attr) is list:
-                for a in attr:
-                    out |= a.value
-            else:
-                out = attr.value
-            return out
-
-        @classmethod
-        def from_num(cls, num) -> list:
-            out = []
-            if num & cls.EQ.value:
-                out.append(cls.EQ)
-            if num & cls.ORD.value:
-                out.append(cls.ORD)
-            return out
-
-
-    def __init__(self, width=32, name='base', attributes=[]):
+    def __init__(self, width=32):
         self.width = width
-        self.name = name
-        self.attributes = attributes
 
     @classmethod
     def for_num(base, num) -> 'Base':
         width = num.bit_length()
-        name = 'int'
-        attributes = [Base.Attributes.EQ, Base.Attributes.ORD]
-        return base(width, name, attributes)
+        return base(width)
 
     def __str__(self):
-        return f'{self.name}-{self.width}'
+        return f'b{self.width}'
 
 class Tuple:
     def __init__(self, *elements):
         # Make sure everything in arguments is an instance of Base
-        self.initialised = False
-
-    def from_list(self, l):
-        assert not self.initialised
-
         all_base = functools.reduce(lambda a,b: a & b,
-                                    map(lambda a: isinstance(a, hop_types.Base), l))
+                                    map(lambda a: isinstance(a, hop_types.Base), elements))
         if not all_base:
             raise TypeError("Must be HoP type") from Exception
 
-        self.elements = tuple(l)
-        self.initialised = True
+        self.elements = elements
 
     def __str__(self):
         out = '('
         for e in self.elements:
             out += f'{str(e)},'
         return out[:-1] + ')'
+
+#class Signature:
+#    def __init__(self, typestr):
