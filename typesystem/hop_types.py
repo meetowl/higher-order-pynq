7085c4379a68cdb155b14c022f5f8a06b9b3d333
meetowl <mrontionov@meetowl.site>
2022-10-15 16:25:58 +0100

    Handy operators for typesystem
    Equality (== and !=), type matching (very primitive), construct tuple
    from an object

--- typesystem/hop_types.py
+++ typesystem/hop_types.py
@@ -10,6 +10,28 @@ class Type:
         return False
     def is_base(self):
         return False
+    def __ne__(self, other):
+        return not (self == other)
+    def __eq__(self, other):
+        return NotImplemented
+
+    # Currently only allow to typecheck tuples
+    # Please re-implement this after you read some theory
+    def typeMatch(self, sigGiven):
+        if self.is_tuple():
+            if not sigGiven.is_tuple():
+                return False
+        else:
+            return False
+
+        for i in range(self.arity):
+            # Only checks one eval level down
+            if self.elements[i].is_base() and sigGiven.elements[i].is_function():
+                if self.elements[i] != sigGiven.elements[i].typeout:
+                    return False
+            elif self.elements[i] != sigGiven.elements[i]:
+                return False
+        return True
 
 class Base(Type):
     def __init__(self, width=32):
@@ -26,11 +48,25 @@ class Base(Type):
     def is_base(self):
         return True
 
+    def __eq__(self, other):
+        return other.is_base() and self.width >= other.width
+
 class Tuple(Type):
     def __init__(self, elementList):
         self.elements = elementList
         self.arity = len(elementList)
         self.empty = self.arity == 0
+
+    @classmethod
+    def from_objects(cls, objectList):
+        '''Get the tuple type from some objects
+           Assumes those objects are children of the HoP typesystem.'''
+        elementList = list()
+        for o in objectList:
+            elementList.append(o.signature)
+
+        return cls(elementList)
+
 
     def __str__(self):
         if self.empty:
@@ -45,6 +81,17 @@ class Tuple(Type):
     def is_tuple(self):
         return True
 
+    def __eq__(self, other):
+        if not other.is_tuple():
+            return False
+
+        for i in range(self.arity):
+            if self.elements[i] != other.elements[i]:
+                return False
+        return True
+
+
+
 class Function(Type):
     def __init__(self, typein, typeout):
         self.typein = typein
@@ -56,6 +103,8 @@ class Function(Type):
     def is_function(self):
         return True
 
+    def __eq__(self, other):
+        return other.is_function() and self.typein != other.typein and self.typeout != other.typeout
 
 def parse(typestr):
     lexer = lex.lex(module = typelexer, debug = False)
