dfa99cf72ae753f40148db5615651dd071422709
meetowl <mrontionov@meetowl.site>
2023-03-12 08:22:00 +0000

    add_reduce works!

--- cores/add_reduce/add_reduce.cpp
+++ cores/add_reduce/add_reduce.cpp
@@ -13,9 +13,8 @@
 #define CALL_COUNT 2
 #define CURRENT_REP_ADDR 3
 #define STATUS 4
-
-// Reset signal for caller
-#define RESET 5
+#define ACCUMULATOR 5
+#define DONE 6
 
 // Endpoint to argument's space
 #define CEP 8
@@ -24,48 +23,39 @@
 #define REP_addr 10
 
 // debug flags
-#define IDLE 0
-#define WAITING_FOR_REP_ADDR 1
+#define IDLE 1
 #define PROCESSING_LIST 2
 
+#define DW 32
+typedef ap_uint<DW> tpkt;
 
 void add_reduce_ppo(volatile int *m_itf,
 		volatile uint32_t regspace[16],
-		hls::stream<ap_axis<32,2,5,6>> &list_in) {
+		hls::stream<tpkt> &list_in) {
 #pragma HLS INTERFACE s_axilite port=regspace bundle=cep
 #pragma HLS INTERFACE s_axilite port=return bundle=cep
-
-	// Removed offset=off
 #pragma HLS INTERFACE mode=m_axi depth=1 port=m_itf bundle=mst
-
-	// Streaming interface
 #pragma HLS INTERFACE axis port=list_in
 
         regspace[STATUS] = IDLE;
 	regspace[SIGNATURE] = 10101;
-	ap_axis<32,2,5,6> num;
 
-        if (regspace[REP_addr] != 0) {
-                // Tell the list stub we are ready to receive communicado
-                m_itf = (volatile int*)regspace[CEP];
-                *m_itf = 1;
+        tpkt next;
+        uint32_t accumulator = 0;
+        while (!regspace[REP_addr]) {
+                if (list_in.read_nb(next)) {
+                        accumulator += next;
+                }
+  tt      }
 
-                list_in.read(num);
-                // Accumulate to existing result
-                uint32_t res = regspace[REP_addr] + num.data;
+        /* Push the result back to the REP */
+        m_itf = (volatile int*)regspace[REP_addr];
+        *m_itf = accumulator;
+        m_itf = (volatile int*)(regspace[REP_addr] + 4);
+        *m_itf = 1;
 
-                // immediate optimisation: don't write result to memory just keep it internal
-                // like this for debugging
-                uint32_t rep_payload[2];
-                rep_payload[0] = res;
-                rep_payload[1] = regspace[CEP_ADDR];
+        regspace[ACCUMULATOR] = 0;
+        regspace[REP_addr] = 0;
 
-                /* Push the result back to the REP */
-                m_itf = (volatile int*)regspace[REP_addr];
-                *m_itf = rep_payload[0];
-                m_itf = (volatile int*)(regspace[REP_addr] + 4);
-                *m_itf = rep_payload[1];
-        }
-        // regspace[REP_addr] = 0;
-        // regspace[CALL_COUNT]++;
+        return;
 }
