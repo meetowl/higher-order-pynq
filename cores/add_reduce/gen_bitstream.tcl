992082fa5d050bd62c50c05129c5d02495772f30
meetowl <mrontionov@meetowl.site>
2023-03-21 03:19:55 +0000

    fix & separate make system

--- cores/add_reduce/gen_bitstream.tcl
+++ cores/add_reduce/gen_bitstream.tcl
@@ -6,7 +6,7 @@ open_bd_design {/home/meetowl/Documents/pynq-stream/pynq-stream.srcs/sources_1/b
 
 # Updating
 update_ip_catalog -rebuild -scan_changes
-report_ip_status -name ip_status
+report_ip_status
 upgrade_ip -vlnv HoP:HoP:HoP_add_reduce:0.1 [get_ips  pynq_stream_HoP_add_reduce_0_0] -log ip_upgrade.log
 export_ip_user_files -of_objects [get_ips pynq_stream_HoP_add_reduce_0_0] -no_script -sync -force -quiet
 
