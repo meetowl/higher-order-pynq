2dd5360c973cbf181047bada414e29be94fbb5ba
meetowl <mrontionov@meetowl.site>
2023-03-21 03:19:20 +0000

    change def names to be more clear

--- cores/add_reduce/add_reduce.cpp
+++ cores/add_reduce/add_reduce.cpp
@@ -14,15 +14,16 @@
 #define DEBUG 3
 #define STATUS 4
 
-// List's CEP Caller Ready address
+// list_in's CEP Caller Ready address
 #define LIST_CREADY 8
 
 // Data channel
-#define REP_ADDR 10
+#define RET_ADDR 10
 
 // debug flags
 #define IDLE 1
 #define PROCESSING_LIST 2
+#define WRITING_RESULT 3
 
 #define DW 32
 typedef ap_uint<DW> tpkt;
@@ -37,28 +38,28 @@ void add_reduce_ppo(volatile int *m_itf,
 
         regspace[STATUS] = IDLE;
 	regspace[SIGNATURE] = 10101;
-        regspace[DEBUG] = 22;
         uint32_t accumulator = 0;
 
-        if (regspace[LIST_CREADY] && regspace[REP_ADDR]) {
-                regspace[STATUS] = 2;
+        if (regspace[RET_ADDR]) {
+                regspace[STATUS] = PROCESSING_LIST;
                 while (!list_in.empty() || regspace[LIST_CREADY]) {
 #pragma HLS PIPELINE II=1
                         tpkt t = 0;
                         if (list_in.read_nb(t)) {
+                                regspace[CALL_COUNT] += 1;
                                 accumulator += t;
                         }
                 }
-                regspace[STATUS] = 3;
+                regspace[STATUS] = WRITING_RESULT;
                 /* Push the result back to the REP */
-                m_itf = (volatile int*)regspace[REP_ADDR];
+                m_itf = (volatile int*)regspace[RET_ADDR];
                 *m_itf = accumulator;
-                m_itf = (volatile int*)(regspace[REP_ADDR] + 4);
+                m_itf = (volatile int*)(regspace[RET_ADDR] + 4);
                 *m_itf = 1;
 
-                regspace[REP_ADDR] = 0;
+                regspace[RET_ADDR] = 0;
                 accumulator = 0;
-                regspace[DEBUG] = regspace[DEBUG] + 1;
+
         }
 
         return;
