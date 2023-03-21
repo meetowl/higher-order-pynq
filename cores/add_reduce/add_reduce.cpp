24add061e60966c8646cb6a3b9aaa0c900c63b09
meetowl <mrontionov@meetowl.site>
2023-03-21 16:37:00 +0000

    add_reduce redesign begining

--- cores/add_reduce/add_reduce.cpp
+++ cores/add_reduce/add_reduce.cpp
@@ -21,7 +21,7 @@
 #define CEP 8
 
 // Data channel
-#define REP_addr 10tt
+#define REP_addr 10
 
 // debug flags
 #define IDLE 0
@@ -41,40 +41,31 @@ void add_reduce_ppo(volatile int *m_itf,
 	// Streaming interface
 #pragma HLS INTERFACE axis port=list_in
 
+        regspace[STATUS] = IDLE;
 	regspace[SIGNATURE] = 10101;
 	ap_axis<32,2,5,6> num;
-	if (regspace[RESET]) {
-		regspace[STATUS] = IDLE;
-		regspace[REP_addr] = 0;
-	} else if ((volatile int*) regspace[REP_addr]) {
-		regspace[STATUS] = WAITING_FOR_REP_ADDR;
-	} else {
-		regspace[STATUS] = PROCESSING_LIST;
-		regspace[CURRENT_REP_ADDR] = regspace[REP_addr];
 
-		// Tell the list stub we are ready to receive communicado
-		m_itf = (volatile int*)regspace[CEP];
-		*m_itf = 1;
+        if (regspace[REP_addr] != 0) {
+                // Tell the list stub we are ready to receive communicado
+                m_itf = (volatile int*)regspace[CEP];
+                *m_itf = 1;
 
-		// Process the list
-		while (!regspace[RESET]) {
-			list_in.read(num);
-			// Accumulate to existing result
-			uint32_t res = regspace[REP_addr] + num.data;
+                list_in.read(num);
+                // Accumulate to existing result
+                uint32_t res = regspace[REP_addr] + num.data;
 
-			// immediate optimisation: don't write result to memory just keep it internal
-			// like this for debugging
-			uint32_t rep_payload[2];
-			rep_payload[0] = res;
-			rep_payload[1] = regspace[CEP_ADDR];
+                // immediate optimisation: don't write result to memory just keep it internal
+                // like this for debugging
+                uint32_t rep_payload[2];
+                rep_payload[0] = res;
+                rep_payload[1] = regspace[CEP_ADDR];
 
-			/* Push the result back to the REP */
-			m_itf = (volatile int*)regspace[REP_addr];
-			*m_itf = rep_payload[0];
-			m_itf = (volatile int*)(regspace[REP_addr] + 4);
-			*m_itf = rep_payload[1];
-		}
-		regspace[CALL_COUNT]++;
-	}
-	return;
+                /* Push the result back to the REP */
+                m_itf = (volatile int*)regspace[REP_addr];
+                *m_itf = rep_payload[0];
+                m_itf = (volatile int*)(regspace[REP_addr] + 4);
+                *m_itf = rep_payload[1];
+        }
+        // regspace[REP_addr] = 0;
+        // regspace[CALL_COUNT]++;
 }
