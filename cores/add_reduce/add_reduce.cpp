dad6e2928fe184e690ee736b2f9a34015e19d82e
meetowl <mrontionov@meetowl.site>
2023-04-04 16:44:39 +0100

    add_reduce completed

--- cores/add_reduce/add_reduce.cpp
+++ cores/add_reduce/add_reduce.cpp
@@ -11,16 +11,14 @@
 #define SIGNATURE 0
 #define CEP_ADDR 1
 #define CALL_COUNT 2
-#define CURRENT_REP_ADDR 3
+#define DEBUG 3
 #define STATUS 4
-#define ACCUMULATOR 5
-#define DONE 6
 
-// Endpoint to argument's space
-#define CEP 8
+// List's CEP Caller Ready address
+#define LIST_CREADY 8
 
 // Data channel
-#define REP_addr 10
+#define REP_ADDR 10
 
 // debug flags
 #define IDLE 1
@@ -39,23 +37,29 @@ void add_reduce_ppo(volatile int *m_itf,
 
         regspace[STATUS] = IDLE;
 	regspace[SIGNATURE] = 10101;
-
-        tpkt next;
+        regspace[DEBUG] = 22;
         uint32_t accumulator = 0;
-        while (!regspace[REP_addr]) {
-                if (list_in.read_nb(next)) {
-                        accumulator += next;
+
+        if (regspace[LIST_CREADY] && regspace[REP_ADDR]) {
+                regspace[STATUS] = 2;
+                while (!list_in.empty() || regspace[LIST_CREADY]) {
+#pragma HLS PIPELINE II=1
+                        tpkt t = 0;
+                        if (list_in.read_nb(t)) {
+                                accumulator += t;
+                        }
                 }
-  tt      }
+                regspace[STATUS] = 3;
+                /* Push the result back to the REP */
+                m_itf = (volatile int*)regspace[REP_ADDR];
+                *m_itf = accumulator;
+                m_itf = (volatile int*)(regspace[REP_ADDR] + 4);
+                *m_itf = 1;
 
-        /* Push the result back to the REP */
-        m_itf = (volatile int*)regspace[REP_addr];
-        *m_itf = accumulator;
-        m_itf = (volatile int*)(regspace[REP_addr] + 4);
-        *m_itf = 1;
-
-        regspace[ACCUMULATOR] = 0;
-        regspace[REP_addr] = 0;
+                regspace[REP_ADDR] = 0;
+                accumulator = 0;
+                regspace[DEBUG] = regspace[DEBUG] + 1;
+        }
 
         return;
 }
